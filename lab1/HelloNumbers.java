7a77f17de852f599fe86a5d9203b0058a0bbb47d
Isaac <isaac@IsaacdeMBP.fios-router.home>
2018-12-27 16:18:50 -0500

    completed first part of LAB1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,18 @@
+public class HelloNumbers {
+	public static void main(String[] args){
+		int x = 0;
+		while (x < 1) {
+			System.out.println(x);
+			x = x + 1;
+		}
+		String y = "numbers";
+		System.out.println(y);
+	}
+}
+
+/*
+
+1. Declare first then use.
+2. Variables type can not be changed.
+3. Types are verified before code runs.
+*/
---
+++ lab1/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+}
+
+/*
+1. All code in Java must be part of a class.
+2. {}
+3. ;
+4. public static void main(String[] args)
+*/
