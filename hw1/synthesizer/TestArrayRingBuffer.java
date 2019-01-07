3654a8d984726f154fa8464ab2a1db5d02a83bff
Isaac <isaac@IsaacdeMBP.fios-router.home>
2019-01-07 02:07:02 -0500

    finish hw1

--- hw1/synthesizer/TestArrayRingBuffer.java
+++ hw1/synthesizer/TestArrayRingBuffer.java
@@ -9,7 +9,26 @@ import static org.junit.Assert.*;
 public class TestArrayRingBuffer {
     @Test
     public void someTest() {
-        //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+        ArrayRingBuffer arb = new ArrayRingBuffer(3);
+        assertTrue(arb.isEmpty());
+        assertFalse(arb.isFull());
+    }
+    @Test
+    public void someTest2() {
+        ArrayRingBuffer arb = new ArrayRingBuffer(3);
+        arb.enqueue(1);
+        arb.enqueue(2);
+        assertFalse(arb.isEmpty());
+        assertFalse(arb.isFull());
+    }
+    @Test
+    public void someTest3() {
+        ArrayRingBuffer arb = new ArrayRingBuffer(3);
+        arb.enqueue(1);
+        arb.enqueue(2);
+        arb.enqueue(3);
+        assertFalse(arb.isEmpty());
+        assertTrue(arb.isFull());
     }
 
     /** Calls tests for ArrayRingBuffer. */
